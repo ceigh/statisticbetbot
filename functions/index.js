45869180c923bb1131c0a099eee612154bc4eb8a
Artjom Löbsack <ceigh@pm.me>
2019-09-23 13:05:23 +0500

    fix: Restore Firebase structure

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
-const web = require('../src/web');
-const bot = require('../src/bot');
+const web = require('./src/web');
+const bot = require('./src/bot');
 
 
 const app = web(bot(functions.config().betmanager.token));
